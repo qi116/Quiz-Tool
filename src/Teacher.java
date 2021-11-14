14374cf4ec3f9d4c4802e107aebbc41a89881a98
boilermaker021 <nathan.leakeas@gmail.com>
2021-11-14 15:20:54 -0500

    Removed unnecessary method

--- src/Teacher.java
+++ src/Teacher.java
@@ -59,15 +59,7 @@ public class Teacher extends Account implements Serializable {
         courses.add(c);
         return c;
     }
-
-    /**
-     * Adds quiz to course object
-     * @param q Quiz to be added
-     * @param c Course to add quiz to
-     */
-    public void addQuizToCourse(Quiz q, Course c) {
-        c.addQuiz(q);
-    }
+    
 
     /**
      * Adds quiz to course in teacher's courses with name
