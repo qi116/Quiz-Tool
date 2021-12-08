6fbe994ca67e5d668c51fb1e5cb428ceab34cc77
Hawkins03 <falcor.peterson@gmail.com>
2021-12-08 14:52:32 -0500

    added request typew

--- src/Message.java
+++ src/Message.java
@@ -7,7 +7,8 @@ public class Message {
         GET, //response: GET, dataType, Quiz if Quiz, String if any other data type
         MODIFY, //response: MODIFY, dataType, success
         REMOVE, //response: REMOVE, dataType,
-        LIST //response: String[] list of names
+        LIST, //response: String[] list of names
+        UPDATE //sending update back to client
     };
 
     public enum dataType {
